7024a193180dd0fb545de48c86f08050013323ba
redouane-hakim <hakim.redouane@ensam-casa.ma>
2025-11-09 17:54:43 +0100

    init proj

---
+++ src/main/java/ma/xproce/gestionevenements/GestionevenementsApplication.java
@@ -0,0 +1,13 @@
+package ma.xproce.gestionevenements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionevenementsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionevenementsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/xproce/gestionevenements/GestionevenementsApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.xproce.gestionevenements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionevenementsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
